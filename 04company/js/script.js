6e6098d33f4ea922b78f755ba237897c207568a5
Your Name <you@example.com>
2022-03-21 03:34:38 +0900

    220321

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -15,6 +15,38 @@ $(document).ready(function(){
     var mySlider1 = $('.slider2').bxSlider({
         pager: false, controls: false, auto:true
     })
+      $(window).scroll(function () {
+        let scrollY = window.pageYOffset;
+        console.log(scrollY)
+        if (scrollY > 200) {
+            $("#header").addClass("fixed");
+        } else {
+            $("#header").removeClass("fixed")
+        }
+    })
+    const $toElem = $(".toggle");
+    let chk = true;
+    $toElem.click(function () {
+        console.log("click")
+        if (chk == true) {
+            $(this).addClass("active");
+            $(".navbar").css({ left: 0 })
+            $(".lang").css("display", "flex");
+            $(".overLayer").show();
+            chk = false;
+        } else {
+            $(this).removeClass("active");
+            $(".navbar").css({ left: "-100%" })
+            $(".lang").hide();
+            chk = true;
+        }
+    })
+    $(window).resize(function () {
+        $(".toggle").removeClass("active");
+        $(".navbar").removeAttr("style");
+        $(".lang").removeAttr("style");
+        $(".overLayer").hide();
+    })
 
 });
 
