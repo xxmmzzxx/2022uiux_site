7f3e79ad583db81f0b555dcac1c50a12d35b64f7
xxmmzzxx <xxmmzzxx@naver.com>
2022-04-06 17:44:42 +0900

    20220406

---
+++ 06fullpage/js/script.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+    navigation: true,
+    responsiveWidth: 700,
+    anchors: ['home', 'about-us', 'contact'],
+    parallax: true,
+    onLeave: function(origin, destination, direction){
+        console.log("Leaving section" + origin.index);
+    },
+});
