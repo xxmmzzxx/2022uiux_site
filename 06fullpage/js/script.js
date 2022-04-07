b4f43e84f0cf67fd8be8e68de20b29324d3cc6d6
xxmmzzxx <xxmmzzxx@naver.com>
2022-04-07 18:23:42 +0900

    20220407

--- 06fullpage/js/script.js
+++ 06fullpage/js/script.js
@@ -7,3 +7,19 @@ new fullpage('#fullpage', {
         console.log("Leaving section" + origin.index);
     },
 });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  autoplay: { delay: 5000, },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+$( document ).ready( function() {
+  $( 'swiper-slide' ).fadeIn( 2000 );
+} );
